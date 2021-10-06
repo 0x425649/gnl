7ffbfd4141482069018460127a0ec7ceb15dcbce
ffmiyo <leffmiyo@gmail.com>
2021-10-06 12:46:38 +0800

    redo with fixed size static holder

--- get_next_line.c
+++ get_next_line.c
@@ -17,63 +17,94 @@ char	*ft_strchr(const char *s, int c)
 	}
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*buf;
+
+	buf = (unsigned char *)s;
+	while (n--)
+		*buf++ = c;
+	return (s);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	if (size != 0)
+	while (*dst && i < size)
 	{
-		while (src[i] && i + 1 < size)
-		{
-			dst[i] = src[i];
-			++i;
-		}
-		dst[i] = 0;
+		++i;
+		++dst;
 	}
-	while (src[i])
+	while (*src && i + 1 < size)
+	{
+		*dst++ = *src++;
+		++i;
+	}
+	if (i < size)
+		*dst = '\0';
+	while (*src++)
 		++i;
 	return (i);
 }
 
-// read into buffer.
-// search for \n.
-// strlcpy into string up to n + 1.
-// TODO: manage buffer.
-// 		 handle weird cases.		 
+// Left-shift src to dst by n.
+// Vacated space on the right set to zero.
+char	*ft_memcpy_rclr(char *dst, char *src, size_t n)
+{
+	char unsigned	*ds;
+	char unsigned	*sr;
+	size_t			len;
+
+	if (!dst && !src)
+		return (NULL);
+	ds = dst;
+	sr = src;
+	len = n;
+	while (n-- > 0)
+		*ds++ = *sr++;
+	while (len-- > 0)
+		*sr-- = '\0';
+	return (dst);
+}
+		 
 char	*get_next_line(int fd)
 {
-	size_t			len;
-	int				r;
-	static int		start;
-	static char		buf[BUFFER_SIZE + 1];
-	char			*str;
-	char			*line_end;
+	char		buf[BUFFER_SIZE];
+	static char	sink[4096];
+	char		*needle;
+	char		*temp;
 
-	if (start == 0)
-		r = read(fd, buf, BUFFER_SIZE);
-		buf[r] = '\0';
-	line_end = ft_strchr(buf, '\n');
-	//printf("buf:\n%s", buf);
-	len = 0;
-	if (line_end == NULL)
+	while(1)
 	{
-		len = BUFFER_SIZE - start;
-		start = 0;
-		r = read(fd, buf, BUFFER_SIZE);
-		buf[r] = '\0';
-		line_end = ft_strchr(buf, '\n');
+		ft_memset(buf, '\0', BUFFER_SIZE);
+		// most likely EOF
+		if (!read(fd, buf, BUFFER_SIZE))
+		{
+			//malloc
+			//return copy sink
+			printf("eof %s\n", sink);
+			return (NULL);
+		}
+		ft_strlcat(sink, buf, 4096);
+		needle = ft_strchr(sink, '\n');
+		// not found
+		if (needle == NULL)
+		{
+			continue ;
+		}
+		// found match
+		break ;
 	}
-	else
-	{
-		// make 1 space for \n
-		len = line_end - buf + 1;
-		// make 1 space for nul-terminate
-		str = malloc(sizeof(char) * len + 1);
-		ft_strlcpy(str , &buf[start], len + 1);
-		start += len;
-	}
-	return (str);
+	temp = malloc((needle - sink) + 1);
+	if (temp == NULL)
+		return (NULL);
+	temp[needle - sink] = '\0';
+	ft_strlcat(temp, sink, (needle - sink) + 1);
+	// shift left by 
+	ft_memcpy_rclr(sink, needle + 1, 4096 - (needle - sink + 1));
+	return (temp);
 }
 
 void main()
@@ -93,36 +124,3 @@ void main()
 	free(str);
 	fclose(out);
 }
-
-// char	*get_next_line(int fd)
-// {
-// 	size_t			len;
-// 	int				r;
-// 	static int		start;
-// 	static char		buf[BUFFER_SIZE + 1];
-// 	char			*str;
-// 	char			*line_end;
-
-// 	while(1)
-// 	{
-// 		r = read(fd, buf, BUFFER_SIZE);
-// 		if (r == 0)
-// 			break ;
-// 		buf[BUFFER_SIZE] = '\0';
-// 		line_end = ft_strchr(buf, '\n');
-// 		len = 0;
-// 		while (line_end == NULL)
-// 		{
-// 			len += BUFFER_SIZE - start;
-// 			start = 0;
-// 			free(str);
-// 			str = (char *)malloc(sizeof(char) * (len + 1))
-// 			r = read(fd, buf, BUFFER_SIZE);
-// 			if (r == 0)
-// 				break ;
-// 			line_end = ft_strchr(buf, '\n');
-// 		}
-
-// 	}
-// 	return (str);
-// }
